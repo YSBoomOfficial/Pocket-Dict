26a61e8ff9467195d81b25a739553b977c24292c
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2021-12-31 20:36:02 +0000

    removed only noun checkbox (see previous commit message) added command param to show_less checkbox so that it calls ssearch_handler() reloading the UI

--- UI.py
+++ UI.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter.constants import BOTH, END, LEFT
+from tkinter.constants import END
 from tkinter.messagebox import showerror
 import search as sh
 
@@ -29,6 +29,53 @@ center_y = int(screen_height/2 - window_height / 2)
 
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
+### handlers ###
+# Display results on screen
+def display_results(results: list[str]):
+    # clear any already displayed results
+    result_list.delete(0, END)
+
+    # add each result to the end of the listbox
+    for i in range(0, len(results)-1):
+        result_list.insert(END, results[i])
+
+# tap handler for clear button
+def clear_handler():
+    result_list.delete(0, END)
+    field_output.set("")
+
+# tap handler for search button
+def search_handler():
+    # show error if field is empty
+    if field_output.get().lower().strip() == "":
+        showerror(
+            title='Error',
+            message="No text to translate."
+        )
+        return
+
+    ##### FILTER NOUN NOT WORKING #####
+    # __only_nouns = True if only_nouns.get() == "1" else False
+
+    # convert StringVar to bool, use private variable to avoid namespace collision
+    __show_less = True if show_less.get() == "1" else False
+
+    ##### FILTER NOUN NOT WORKING #####
+    # data = sh.get_result(field_output.get().lower().strip(), __only_nouns, __show_less)
+
+    data = sh.get_result(field_output.get().lower().strip(), __show_less)
+
+    # show error if data returned is empty
+    if data == []:
+        showerror(
+            title='Oops! Something went wrong',
+            message=f"No translation found for \"{field_output.get()}\".\nTry unchecking some of the checkboxes to widen the scope."
+        )
+        return
+    else:
+        display_results(data)
+
+### UI components ###
 # create listbox for results
 result_list = tk.Listbox(frame)
 
@@ -49,57 +96,12 @@ field = ttk.Entry(
 field.focus()
 
 
-def display_results(results: list[str]):
-    for i in range(0, len(results)-1):
-        result_list.insert(END, results[i])
-
-# tap handler for search button
-
-
-def search_handler():
-    # show error if field is empty
-    if field_output.get() == "":
-        showerror(
-            title='Error',
-            message="No text to translate."
-        )
-        return
-
-    # convert StringVar to bool, use private variable to avoid namespace collision
-    __only_nouns = True if only_nouns.get() == "1" else False
-    __show_less = True if show_less.get() == "1" else False
-
-    data = sh.get_result(field_output.get(), __only_nouns, __show_less)
-
-    print(field_output.get())
-    print(__only_nouns)
-    print(__show_less)
-
-    # show error if data returned is empty
-    if data == []:
-        showerror(
-            title='Oops! Something went wrong',
-            message=f"No translation found for \"{field_output.get()}\".\nTry unchecking some of the checkboxes to widen the scope."
-        )
-        return
-    else:
-        print(data)
-        display_results(data)
-
-
 # search button
 search_btn = ttk.Button(
     frame,
     text="Search",
 )
 search_btn.configure(command=search_handler)
-
-# tap handler for clear button
-
-
-def clear_handler():
-    result_list.delete(0, END)
-    field_output.set("")
 
 
 # clear button
@@ -115,17 +117,19 @@ less_box = ttk.Checkbutton(
     text='Show Fewer Translations',
     variable=show_less,
     onvalue=True,
-    offvalue=False
+    offvalue=False,
+    command=search_handler
 )
 
+##### FILTER NOUN NOT WORKING #####
 # checkbox for only nouns
-noun_box = ttk.Checkbutton(
-    frame,
-    text='Only Show Nouns',
-    variable=only_nouns,
-    onvalue=True,
-    offvalue=False
-)
+# noun_box = ttk.Checkbutton(
+#     frame,
+#     text='Only Show Nouns',
+#     variable=only_nouns,
+#     onvalue=True,
+#     offvalue=False
+# )
 
 # pack all the widgets
 intro_label.pack(**options)
@@ -133,8 +137,15 @@ field.pack(**options)
 search_btn.pack(**options)
 clear_btn.pack(**options)
 less_box.pack(**options)
-noun_box.pack(**options)
+
+##### FILTER NOUN NOT WORKING #####
+# noun_box.pack(**options)
+
 result_list.pack(**options)
+
 # add frame to root
 frame.pack(**options)
+
+
+# def main():
 root.mainloop()
